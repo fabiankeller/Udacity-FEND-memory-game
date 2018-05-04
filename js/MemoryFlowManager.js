467bccb90d5d83316aace75b4577fb73860694af
Fabian Keller <fabk@zuehlke.com>
2018-05-04 15:30:03 +0200

    feat: provide restart functionality

--- js/MemoryFlowManager.js
+++ js/MemoryFlowManager.js
@@ -1,6 +1,7 @@
 class MemoryFlowManager {
   constructor(deckId) {
     this.deck = document.getElementById(deckId);
+    this.solvedMessage = document.getElementById('memorySolved');
     this.moves = 0;
     this.openCards = [];
     this.matchedCards = [];
@@ -11,16 +12,22 @@ class MemoryFlowManager {
     for (let cardElement of cardElements) {
       cardElement.addEventListener('click', this.handleClickEventOnCard.bind(this));
     }
-    document.getElementById('restartButton').addEventListener('click', () => {
-      const symbols = ['diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube',
-                        'leaf', 'bicycle', 'bomb'];
-      symbols.forEach((symbol) => {
-        const elements = document.getElementsByClassName('fa-' + symbol);
-        for (let element of elements) {
-          element.parentNode.dispatchEvent(new MouseEvent('click'));
-        }
-      });
-    });
+    // document.getElementById('restartButton').addEventListener('click', this.restartMemory.bind(this));
+    // for developing purpose - solve memory automatically
+    // document.getElementById('restartButton').addEventListener('click', () => {
+    //   if (this.matchedCards.length !== 16) {
+    //     const symbols = ['diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube',
+    //                       'leaf', 'bicycle', 'bomb'];
+    //     symbols.forEach((symbol) => {
+    //       const elements = document.getElementsByClassName('fa-' + symbol);
+    //       for (let element of elements) {
+    //         element.parentNode.dispatchEvent(new MouseEvent('click'));
+    //       }
+    //     });
+    //   } else {
+    //     this.restartMemory();
+    //   }
+    // });
 
   }
 
@@ -56,29 +63,45 @@ class MemoryFlowManager {
     card.setAttribute('class', 'card show open');
   }
 
+  coverCard(card) {
+    card.setAttribute('class', 'card');
+  }
+
+  markCardAsMatch(card) {
+    card.setAttribute('class', 'card show match');
+  }
+
   coverOpenCards() {
     this.openCards.forEach((card) => {
-      setTimeout(() => {card.setAttribute('class', 'card')}, 1000);
+      setTimeout(this.coverCard.bind(this, card), 1000);
     });
     this.openCards = [];
   }
 
   markOpenCardsAsMatch() {
-    this.matchedCards.push(this.openCards[0]);
-    this.matchedCards.push(this.openCards[1]);
     this.openCards.forEach((card) => {
-      setTimeout(() => {card.setAttribute('class', 'card show match')}, 1000);
+      setTimeout(this.markCardAsMatch.bind(this, card), 1000);
     });
     if (this.matchedCards.length === 16) {
-      setTimeout(this.memorySolved.bind(this), 1000);
+      setTimeout(this.memorySolved.bind(this), 2000);
     }
+    this.matchedCards.push(this.openCards[0]);
+    this.matchedCards.push(this.openCards[1]);
     this.openCards = [];
   }
 
   memorySolved() {
     this.deck.style.display = 'none';
-    const solvedMessage = document.getElementById('memorySolved');
-    solvedMessage.style.display = 'block';
+    this.solvedMessage.style.display = 'block';
+  }
+
+  restartMemory() {
+    this.openCards.forEach(this.coverCard);
+    this.matchedCards.forEach(this.coverCard);
+    this.matchedCards = [];
+    this.openCards = [];
+    this.deck.style.display = 'flex';
+    this.solvedMessage.style.display = 'none';
   }
 
 }
--- js/app.js
+++ js/app.js
@@ -17,3 +17,7 @@ memoryDeck.initDeck();
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+document.getElementById('restartButton').addEventListener('click', () => {
+  memoryDeck.initDeck();
+});
